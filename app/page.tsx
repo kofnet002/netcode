d8509e0c160cdbd21e9732ba0213a7181a7dec76
frimpong-kofi <noeljoel61@gmail.com>
2023-05-04 00:36:28 +0000

    +: Toggle theme mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
 import AddCode from "./components/AddCode";
 import CodeList from "./components/CodeList";
-import { getAllCodes } from "./api/hello/codes/route";
-import { ICode } from "@/types/codes";
 import { getNotes } from "@/api";
+import Navbar from "./components/Navbar";
 
 const HomePage = async () => {
-
   const codes = await getNotes();
-  // console.log("All codes", codes)
   return (
-    <main className="max-w-4xl mx-auto mt-4">
-      <div className="text-center my-5 flex flex-col gap-4">
-        <h1 className="text-2xl font-bold">Codes</h1>
-        <AddCode />
-      </div>
-      <CodeList codes={codes} />
-    </main>
+    <>
+      <Navbar />
+      <main className="max-w-4xl mx-auto mt-4">
+        <div className="text-center my-5 flex flex-col gap-4">
+          <h1 className="text-2xl font-bold my-3">My Notes</h1>
+          <AddCode />
+        </div>
+        <CodeList codes={codes} />
+      </main>
+    </>
   );
 };
 
